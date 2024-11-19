0c5fe3f414e4dfe0866dd58d87b36cd0c6902be3
Tonmay <getready5169@gmail.com>
2024-11-19 19:43:57 +0600

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import './App.css'
 
 function App() {
 
+  function handleClick() {
+    alert('Button clicked')
+    console.log('Button clicked')
+  }
+  
+  const addToFive = (num) => {
+    alert(num + 5)
+  }
+
   return (
     <>
       <h1>React core concepts 2</h1>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => {
+        alert('Button clicked2')
+        console.log('Button clicked2')
+      }}>Click2</button>
+      {/* bhejailla */}
+      <button onClick={() => addToFive(4)}>Four</button>
     </>
   )
 }
